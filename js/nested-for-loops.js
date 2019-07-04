fa29c67369885158519e548eec07fa74f549dead
Calvin <calmoon808@gmail.com>
2019-07-03 21:34:22 -1000

    nested loops

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -4,11 +4,20 @@
  * @param {number} [width=depth] 
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
-  let result = "\n";
+function nestedForLoops (depth, width=depth) {
+  let result = '\n';
 
   // Write code here
-
+  for (let i = 0; i < depth; i++){
+    for (let j = 0; j < width; j++){
+      result += '{x:' + j + ', y:' + i + '}';
+      if (j < width - 1){
+        result += ', ';
+      }
+    }
+    result += '\n';
+  }
+  return result;
 };
 
 // To see your console output outside the tests add function calls here.
